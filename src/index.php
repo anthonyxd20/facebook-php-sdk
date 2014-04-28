2d5e4662b8d4afb426ff0807d5164d812c9e993f
anthonyxd20 <anthony_20_20_20@hotmail.com>
2014-04-28 08:40:55 -0600

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+require 'facebook-php-sdk/src/facebook.php';
+
+// Instanciamos un nuevo objeto de Facebook, con los parametros de appId y Secret
+$facebook = new Facebook(array(
+  'appId'  => '822018101160502',
+  'secret' => '17f853ccb5f5b2cbddeda41bee499df1',
+  'cookie' => true,
+)); 
